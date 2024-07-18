87d9f476ae78e769de388b0181f6f44ee262dc37
Thomas Damiani <thomas5d@comcast.net>
2024-07-18 16:57:28 -0400

    start of emulator comms

--- lib/shell_utils.py
+++ lib/shell_utils.py
@@ -2,6 +2,7 @@ import signal
 import sys
 
 import serial
+import socket
 
 from lib.database_utils import initialize_database
 from lib.radio_utils import initialize_radio, unpack_message
@@ -11,6 +12,11 @@ def hard_exit(radiohead, db, signum, frame):
     print("hard exit")
     radiohead.radio.close()
     db.client.close()
+    sys.exit(0)
+
+
+def socket_exit(socket, signum, frame):
+    socket.close()
     sys.exit(0)
 
 
@@ -52,7 +58,27 @@ def send_command_serial():
 
 
 def receive_loop_emulator():
-    pass
+    # emulator will send data via a socket on port 5000
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((socket.gethostname(), 5000))
+
+    database = initialize_database('heartbeats', "emulator")
+
+    while True:
+        serversocket.listen()
+        (clientsocket, address) = serversocket.accept()
+        with clientsocket:
+            print(f"connected to {address}")
+            while True:
+                data = clientsocket.recv(256)
+                if not data:
+                    break
+                print(data)
+                # res = unpack_message(data)
+                # if res is not None:
+                #     id, time, data = res
+                #     print(id, time, data)
+                #     database.upload_data(id, time, data)
 
 
 def send_command_emulator():
