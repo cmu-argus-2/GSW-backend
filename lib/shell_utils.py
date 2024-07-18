d7fe4ee07c5d3fffda99d4ec80a9f6007c6ca794
Thomas Damiani <thomas5d@comcast.net>
2024-07-18 14:52:12 -0400

    bug fix in exit function

--- lib/shell_utils.py
+++ lib/shell_utils.py
@@ -5,15 +5,15 @@ from lib.radio_utils import initialize_radio, unpack_message
 from lib.database_utils import initialize_database
 
 
-def hard_exit(radio, db, signum, frame):
+def hard_exit(radiohead, db, signum, frame):
     print("hard exit")
-    radio.close()
+    radiohead.radio.close()
     db.client.close()
     sys.exit(0)
 
 
 def receive_loop():
-    radio = initialize_radio()
+    radiohead = initialize_radio()
 
     point_prompt = "Tag for stored data (leave blank for argus-1):"
     point = input(point_prompt) or "argus-1"
@@ -21,12 +21,12 @@ def receive_loop():
 
     signal.signal(
         signal.SIGINT,
-        lambda signum, frame: hard_exit(radio, database, signum, frame)
+        lambda signum, frame: hard_exit(radiohead, database, signum, frame)
     )
 
     while True:
         print("waiting for packet...")
-        msg = radio.receive_message()
+        msg = radiohead.receive_message()
         if msg is not None:
             res = unpack_message(msg)
             if res is not None:
