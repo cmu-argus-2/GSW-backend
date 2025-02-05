460f49e9f00a1a9c8667a912a4185d9236a77695
Your Name <s026134@students.lmsd.org>
2025-02-05 17:53:03 -0500

    made function for determining if there are commands available in the tx table

--- lib/database/db_services.py
+++ lib/database/db_services.py
@@ -30,6 +30,14 @@ def remove_latest_command():
     )
     return result[0] if result else None
 
+def commands_available():
+    """Determine if there are commands available and queued up"""
+    result = query(
+        """
+        SELECT EXISTS (SELECT 1 FROM rxData_tb LIMIT 1);
+        """
+    )
+    return result
 
 def handle_command_ACK(ack):
     """If SC sends an ack for a command, we need to handle successful and failed executions of commands.
@@ -55,27 +63,6 @@ def add_Telemetry(msg_data):
         ('SAT_HEARTBEAT', MSG_ID.SAT_HEARTBEAT, 'telemetry', msg_data) 
     )
 
-    #Hard coded values
-    # result = query(
-    #     """
-    #     INSERT INTO rxData_tb (rx_name, rx_id, rx_type, rx_data)
-    #     VALUES (
-    #         'SAT_HEARTBEAT',
-    #         1, 
-    #         'telemetry',
-    #         '{
-    #             "msg_id": 2,
-    #             "seq_cnt": 1,
-    #             "size": 1,
-    #             "CDH": [0, 0, 0, 0, 0, 0, 1, 1],
-    #             "EPS": [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-    #             "ADCS": [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-    #             "GPS": [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-    #             "THERMAL": [0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
-    #         }'::jsonb
-    #     );
-    # """
-    # )
     
 def add_Ack(msg_data=None):
     result = query(
@@ -106,21 +93,6 @@ def add_File_Meta_Data(msg_data=None):
         """,
         ('SAT_FILE_METADATA', MSG_ID.SAT_FILE_METADATA, 'file', msg_data)
     )
-    
-# def to_json(self):
-#     """ Convert the unpacked telemetry data to JSON format """
-#     telemetry_data = {
-#         "msg_id": self._msg_id,
-#         "seq_cnt": self._seq_cnt,
-#         "size": self._size,
-#         "CDH": self._data_CDH,
-#         "EPS": self._data_EPS,
-#         "ADCS": self._data_ADCS,
-#         "GPS": self._data_GPS,
-#         "THERMAL": self._data_THERMAL,
-#     }
-#     return json.dumps(telemetry_data, indent=4)
-
 
 def add_downlink_data(data_type, args_list=None):
     """Handle adding data that was downlinked to the database (RX table)"""
