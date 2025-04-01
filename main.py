8833eb8f3a2988c718e69805c647d1e1ba84d69a
Swati Anshu <swatianshu27@gmail.com>
2025-04-01 18:38:13 -0400

    main.py changes

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
 import sys
 
-from lib.shell_utils import (receive_loop, receive_loop_emulator,
-                             receive_loop_serial, send_command,
-                             send_command_emulator, send_command_serial)
+from lib.shell_utils import (receive_loop, transmit_loop)
 
 while True:
     connection_prompt = """
-Are you connecting to the satellite via:
- - (r) Radio Frequency
- - (t) tty/serial
- - (e) emulated/simulated satellite
- - (q) quit
+
+
+What operation mode do you want?
+(r) Normal Operation [Downlink and Uplink Functionality]
+(t) Only Transmit Operation 
+(q) quit   
 : """
     conn_type = input(connection_prompt)
     if conn_type == "q":
         sys.exit(0)
 
-    for_prompt = """
-Are you...
- - (r) receive heartbeats from satellite
- - (c) sending a command to the satellite
- - (q) quit
-: """
-    reason = input(for_prompt)
-    if reason == "q":
-        sys.exit(0)
-
     options = {
-        "r": {"r": receive_loop, "c": send_command},
-        "t": {
-            "r": receive_loop_serial,
-            "c": send_command_serial,
-        },
-        "e": {
-            "r": receive_loop_emulator,
-            "c": send_command_emulator,
-        },
+        "r": receive_loop, 
+        "t" : transmit_loop,
     }
 
-    options["r"]["r"]()
+    options[conn_type]()
