0a4ae06dea38d5ff1f3101859c96a0d984dc3500
hazcoper <jose.anca.mateus@tecnico.ulisboa.pt>
2026-02-20 18:20:33 +0000

    chore(transaction_middleware): added option to save the transaction to disk

--- lib/telemetry/transaction_middleware.py
+++ lib/telemetry/transaction_middleware.py
@@ -5,15 +5,68 @@ when the ground station gets a command to create a transaction this class will b
 when a transaction packet is received this class will also be called and deal with the packet
 
 It will eventually also deal with storing the transaction in storage
+    the main idea behind this is to minimize the chances of losing a transaction. it will allow you to recover lost transactions
 
 """
+import pickle
+import json
 from lib.telemetry.splat.splat.transport_layer import TransactionManager, Fragment, Command
-
+import os
+import time
 
 
 class TransactionMiddleware:
-    def __init__(self):
+    """
+    When this module is created, inside the transaction folder it will create a new folder with the timestamp
+    this will server as like sessions
+    Inside this folder it will create a json file for each of the transactions containing all of the information
+        and an associated pickle file that will contain the dict with the fragments received so far
+    """
+    
+    def __init__(self, storage_folder="transactions"):
+        self.storage_folder = storage_folder
         self.transaction_manager = TransactionManager()
+        
+        # check to see if the storage folder exists, if not create it
+        if not os.path.exists(self.storage_folder):
+            os.makedirs(self.storage_folder)
+            
+        # create the session folder with the timestamp
+        self.session_folder = os.path.join(self.storage_folder, f"session_{int(time.time())}")
+        os.makedirs(self.session_folder)
+        
+    def dump_transaction(self, transaction, json_file_path):
+        """
+        Docstring for dump_transaction
+        
+        :param self: Description
+        :param transaction: Description
+        :param json_file_path: Description
+        """
+
+        transaction_dict = {
+            "tid": transaction.tid,
+            "file_path": transaction.file_path,
+            "state": transaction.state,
+            "number_of_packets": transaction.number_of_packets,
+            "len_missing_fragments": len(transaction.missing_fragments),
+            "missing_fragments": transaction.missing_fragments,
+            "start_date": transaction.start_date,
+        }
+        
+        # dump the json file
+        with open(json_file_path, "w") as f:
+            json.dump(transaction_dict, f, indent=4)
+    
+        # check if we have received any packets yet
+        if len(transaction.fragment_dict) == 0:
+            return
+    
+        # dump the pickle file
+        # we will just change the extension to .pkl and dump the missing packets dict
+        pickle_file_path = json_file_path.replace(".json", ".pkl")
+        with open(pickle_file_path, "wb") as f:
+            pickle.dump(transaction.fragment_dict, f)
     
     def process_create_trans(self, cmd):
         """
@@ -45,6 +98,12 @@ class TransactionMiddleware:
         if transaction is None:
             print(f"Failed to create transaction for command: {cmd}")
             return False
+        
+        # create the json with the transaction information
+        json_file_name = f"{tid}_{transaction.start_date}.json"
+        json_file_path = os.path.join(self.session_folder, json_file_name)
+        self.dump_transaction(transaction, json_file_path)
+        
         print(f"Created transaction for command: {cmd} with tid {tid} and file path {file_path}")
         # was able to create the transaction 
         return True
@@ -81,7 +140,7 @@ class TransactionMiddleware:
         
         # change the state of the transaction
         transaction.change_state(2)   # [check] - do we need the states? if so remove the hardcode here
-        
+        self.dump_transaction(transaction, os.path.join(self.session_folder, f"{tid}_{transaction.start_date}.json"))
         return True
     
     def process_fragment(self, frag):
@@ -104,6 +163,9 @@ class TransactionMiddleware:
         
         # add the fragment to the transaction
         is_completed = transaction.add_fragment(frag)
+        
+        # save the transaction information in the json and pickle file
+        self.dump_transaction(transaction, os.path.join(self.session_folder, f"{tid}_{transaction.start_date}.json"))
         
         if is_completed:
             #print in green
