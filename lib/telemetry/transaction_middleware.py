602f24c4a959863914519d87e7994f9ab14d7b47
hazcoper <jose.anca.mateus@tecnico.ulisboa.pt>
2026-02-24 13:07:54 +0000

    feat(transaction_middleware): added thread to save transactions

--- lib/telemetry/transaction_middleware.py
+++ lib/telemetry/transaction_middleware.py
@@ -7,12 +7,18 @@ when a transaction packet is received this class will also be called and deal wi
 It will eventually also deal with storing the transaction in storage
     the main idea behind this is to minimize the chances of losing a transaction. it will allow you to recover lost transactions
 
+
+Storing the transactions in storage is something that takes a bit of time, and I do not want to slow down the receiving 
+of data from the Satellite. So there will be a thread that will be responsible for writing thigns to storage and when we receive something we have 
 """
+
 import pickle
 import json
 from lib.telemetry.splat.splat.transport_layer import TransactionManager, Fragment, Command
 import os
 import time
+import threading
+import queue
 
 
 class TransactionMiddleware:
@@ -34,16 +40,49 @@ class TransactionMiddleware:
         # create the session folder with the timestamp
         self.session_folder = os.path.join(self.storage_folder, f"session_{int(time.time())}")
         os.makedirs(self.session_folder)
-        
+
+        # queue for dumping transactions to storage in the background
+        # each item in the queue is a tuple of (transaction, json_file_path)
+        self._dump_queue = queue.Queue()
+        self._dump_thread = threading.Thread(target=self._dump_worker, daemon=True)
+        self._dump_thread.start()
+
+    def _dump_worker(self):
+        """
+        Background worker thread that processes the dump queue.
+        Runs forever (daemon thread - dies when the main process exits).
+        Pulls (transaction, json_file_path) tuples off the queue and writes them to disk.
+        """
+        while True:
+            try:
+                transaction, json_file_path = self._dump_queue.get()
+                self.dump_transaction(transaction, json_file_path)
+                self._dump_queue.task_done()
+            except Exception as e:
+                print(f"[DumpWorker] Error writing transaction to storage: {e}")
+
+    def _enqueue_dump(self, transaction, json_file_path):
+        """
+        Enqueue a transaction to be dumped to storage by the background thread.
+        This is non-blocking - the caller returns immediately.
+        """
+        self._dump_queue.put((transaction, json_file_path))
+
+    def wait_for_pending_dumps(self):
+        """
+        Block until all queued dump operations have been written to disk.
+        Useful for graceful shutdown or when you need to ensure all data is flushed.
+        """
+        self._dump_queue.join()
+
     def dump_transaction(self, transaction, json_file_path):
         """
-        Docstring for dump_transaction
-        
-        :param self: Description
-        :param transaction: Description
-        :param json_file_path: Description
-        """
+        Writes transaction metadata to a JSON file and received fifragments
+        to an associated pickle file. Called by the background dump worker.
 
+        :param transaction: The transaction object to serialize.
+        :param json_file_path: Path for the output JSON file.
+        """
         transaction_dict = {
             "tid": transaction.tid,
             "file_path": transaction.file_path,
@@ -54,16 +93,12 @@ class TransactionMiddleware:
             "start_date": transaction.start_date,
         }
         
-        # dump the json file
         with open(json_file_path, "w") as f:
             json.dump(transaction_dict, f, indent=4)
     
-        # check if we have received any packets yet
         if len(transaction.fragment_dict) == 0:
             return
     
-        # dump the pickle file
-        # we will just change the extension to .pkl and dump the missing packets dict
         pickle_file_path = json_file_path.replace(".json", ".pkl")
         with open(pickle_file_path, "wb") as f:
             pickle.dump(transaction.fragment_dict, f)
@@ -83,39 +118,30 @@ class TransactionMiddleware:
             print(f"Invalid command name for creating transaction: {cmd.name}")
             return
         
-        
         tid = cmd.arguments.get("tid")
         file_path = cmd.arguments.get("string_command")
-        
         
         if tid is None or file_path is None:
             print("Invalid command arguments for creating transaction")
             print(f"Received cmd_arguments: {cmd.arguments}")
             return False
 
-        # create the transaction
         transaction = self.transaction_manager.create_transaction(tid=tid, file_path=file_path, is_tx=False)
         if transaction is None:
             print(f"Failed to create transaction for command: {cmd}")
             return False
         
-        # create the json with the transaction information
-        json_file_name = f"{tid}_{transaction.start_date}.json"
-        json_file_path = os.path.join(self.session_folder, json_file_name)
-        self.dump_transaction(transaction, json_file_path)
+        json_file_path = os.path.join(self.session_folder, f"{tid}_{transaction.start_date}.json")
+        self._enqueue_dump(transaction, json_file_path)
         
         print(f"Created transaction for command: {cmd} with tid {tid} and file path {file_path}")
-        # was able to create the transaction 
         return True
     
     def process_init_trans(self, cmd):
         """
         After gs sending CREATE_TRANS command, the satellite will respond with a INIT command containing more information
-        about the transaction. This function will deal with that
-        the extra information provided is the hash and the number of packets
-        but for now we are ignoring the hash
-        
-        the command passed here should be the INIT_TRANS command
+        about the transaction. This function will deal with that.
+        The extra information provided is the hash and the number of packets.
         """
         
         if not isinstance(cmd, Command):
@@ -129,48 +155,41 @@ class TransactionMiddleware:
         tid = cmd.get_argument("tid")
         number_of_packets = cmd.get_argument("number_of_packets")
         
-        # find the transaction with the tid
         transaction = self.transaction_manager.get_transaction(tid=tid, is_tx=False)
         if transaction is None:
             print(f"Failed to find transaction with tid {tid} for initializing transaction")
             return False
         
-        # update the transaction with the number of packets
         transaction.set_number_packets(number_of_packets)
-        
-        # change the state of the transaction
-        transaction.change_state(2)   # [check] - do we need the states? if so remove the hardcode here
-        self.dump_transaction(transaction, os.path.join(self.session_folder, f"{tid}_{transaction.start_date}.json"))
+        transaction.change_state(2)
+
+        json_file_path = os.path.join(self.session_folder, f"{tid}_{transaction.start_date}.json")
+        self._enqueue_dump(transaction, json_file_path)
         return True
     
     def process_fragment(self, frag):
         """
-        Once the transaction has been initialized the gs can request packets from the satellite
-        the satellite will respond with fragments packets containing the data
-        this function will process the received fragment and add it to the transaction
+        Once the transaction has been initialized the gs can request packets from the satellite.
+        The satellite will respond with fragment packets containing the data.
+        This function processes the received fragment and adds it to the transaction.
         """
         if not isinstance(frag, Fragment):
             print(f"Invalid fragment type for processing fragment: {type(frag)}")
             return False
         
-        tid = frag.tid   # need to tid to find the transaction
+        tid = frag.tid
         
-        # find the transaction with the tid
         transaction = self.transaction_manager.get_transaction(tid=tid, is_tx=False)
         if transaction is None:
             print(f"Failed to find transaction with tid {tid} for processing fragment")
             return False
         
-        # add the fragment to the transaction
         is_completed = transaction.add_fragment(frag)
-        
-        # save the transaction information in the json and pickle file
-        self.dump_transaction(transaction, os.path.join(self.session_folder, f"{tid}_{transaction.start_date}.json"))
+
+        json_file_path = os.path.join(self.session_folder, f"{tid}_{transaction.start_date}.json")
+        self._enqueue_dump(transaction, json_file_path)
         
         if is_completed:
-            #print in green
-            
             print(f"\033[32mTransaction with tid {tid} is completed, all fragments received\033[0m")
             transaction.write_file("downlinked_data")        
-        return True
-        
+        return True        
