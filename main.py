c8a9df3b28216f24648e5fa62c796c0edd9ef48b
hazcoper <jose.anca.mateus@tecnico.ulisboa.pt>
2026-04-15 20:46:30 +0100

    chore(general): added http server to make it easier to access the data

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import sys
-
+import threading
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from lib.shell_utils import op_mode
 
 
+def run_http_server():
+    server_address = ("0.0.0.0", 8000)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print("HTTP server running on port 8000...")
+    httpd.serve_forever()
+
+
+# Start server in background thread
+server_thread = threading.Thread(target=run_http_server, daemon=True)
+server_thread.start()
+
+
+# Your CLI loop (unchanged)
 while True:
     connection_prompt = """
 What operation mode do you want?
@@ -11,6 +25,7 @@ What operation mode do you want?
 (q) quit   
 Input:  """
     conn_type = input(connection_prompt)
+
     if conn_type == "q":
         sys.exit(0)
 
@@ -19,4 +34,3 @@ Input:  """
     }
 
     options[conn_type]()
-
