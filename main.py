8308186af14fba76aea1163ed3103158ef1665e1
hazcoper <jose.anca.mateus@tecnico.ulisboa.pt>
2026-04-23 11:59:22 +0100

    chore(general): added a digipeater test mode to shell utils

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
 
 
-from lib.shell_utils import op_mode
+from lib.shell_utils import op_mode, digipeater_test
 
 
 while True:
     connection_prompt = """
 What operation mode do you want?
 (o) Operations Mode 
+(d) Digipeater Test Mode
 (q) quit   
 Input:  """
     conn_type = input(connection_prompt)
@@ -15,7 +16,8 @@ Input:  """
         sys.exit(0)
 
     options = {
-        "o": op_mode
+        "o": op_mode,
+        "d": digipeater_test
     }
 
     options[conn_type]()
